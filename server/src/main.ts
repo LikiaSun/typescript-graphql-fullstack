938ba9f57e956847832f79aa2dae2c99ef7c298a
likia <likia0430@outlook.com>
2019-10-20 21:47:45 +0800

    [Develop]TypeORM can connect postgres

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,21 @@ import http from "http";
 
 import app from "./app";
 import { createConnection } from "typeorm";
+import { User } from "./entities/User";
 
 const httpServer = http.createServer(app);
 
 const main = async () => {
-  await createConnection();
+  await createConnection({
+    type: "postgres",
+    host: "localhost",
+    port: 54320,
+    username: "postgres",
+    password: "postgres",
+    database: "noul",
+    synchronize: true,
+    entities: [User],
+  });
   httpServer.listen(app.get("port"), () => {
     console.log(`Server is running on port 9999`);
   });
